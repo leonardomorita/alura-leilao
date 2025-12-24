0c0b66b6ce2fd44a086c52f2fbd8613de8a3d841
leonardomorita <leonardomorita2@gmail.com>
2025-12-24 00:56:08 -0300

    ✨ Aula 3 - Classes de equivalência

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -2,16 +2,20 @@
 
 namespace Alura\Leilao\Service;
 
+use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
     private $maiorValor = -INF;
     private $menorValor = INF;
+    private $maioresLances;
 
     public function avalia(Leilao $leilao): void
     {
-        foreach ($leilao->getLances() as $lance) {
+        $lances = $leilao->getLances();
+
+        foreach ($lances as $lance) {
             $valorDoLance = $lance->getValor();
             if ($valorDoLance > $this->maiorValor) {
                 $this->maiorValor = $valorDoLance;
@@ -21,6 +25,12 @@ class Avaliador
                 $this->menorValor = $valorDoLance;
             }
         }
+
+        usort($lances, function (Lance $lance1, Lance $lance2) {
+            return $lance2->getValor() - $lance1->getValor();
+        });
+
+        $this->maioresLances = array_slice($lances, 0, 3);
     }
 
     public function getMaiorValor(): float
@@ -31,5 +41,13 @@ class Avaliador
     public function getMenorValor(): float
     {
         return $this->menorValor;
+    }
+
+    /**
+     * @return Lance[]
+     */
+    public function getMaioresLances(): array
+    {
+        return $this->maioresLances;
     }
 }
--- tests/Service/AvaliadorTest.php
+++ tests/Service/AvaliadorTest.php
@@ -101,4 +101,33 @@ class AvaliadorTest extends TestCase
         // Verificação (Assert) ou (Then)
         self::assertEquals(2000, $menorValor);
     }
+
+    public function testAvaliadorDeveBuscar3MaioresValores()
+    {
+        // Padrões Arrange-Act-Assert e GivenWhenThen
+
+        // Cenário (Arrange) ou (Given)
+        $leilao = new Leilao('Fiat 147 0KM');
+
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('João');
+        $ana = new Usuario('Ana');
+        $jorge = new Usuario('Jorge');
+
+        $leilao->recebeLance(new Lance($ana, 1500));
+        $leilao->recebeLance(new Lance($joao, 1000));
+        $leilao->recebeLance(new Lance($maria, 2000));
+        $leilao->recebeLance(new Lance($jorge, 1700));
+
+        // Ação a ser testada (Act) ou (When)
+        $leiloeiro = new Avaliador();
+        $leiloeiro->avalia($leilao);
+
+        // Verificação (Assert) ou (Then)
+        $maioresValores = $leiloeiro->getMaioresLances();
+        self::assertCount(3, $maioresValores);
+        self::assertEquals(2000, $maioresValores[0]->getValor());
+        self::assertEquals(1700, $maioresValores[1]->getValor());
+        self::assertEquals(1500, $maioresValores[2]->getValor());
+    }
 }
