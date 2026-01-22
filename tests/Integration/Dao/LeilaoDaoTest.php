c51549608c25d104a9fc3c8e05de33264c2c4876
leonardomorita <leonardomorita2@gmail.com>
2026-01-21 22:41:21 -0300

    ✨ Aula 2 - Garantindo a integridade

--- tests/Integration/Dao/LeilaoDaoTest.php
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -3,23 +3,49 @@
 namespace Alura\Leilao\Tests\Integration\Dao;
 
 use Alura\Leilao\Dao\Leilao as LeilaoDao;
-use Alura\Leilao\Infra\ConnectionCreator;
 use Alura\Leilao\Model\Leilao;
 use PHPUnit\Framework\TestCase;
 
 class LeilaoDaoTest extends TestCase
 {
+    /** @var \PDO $pdo */
+    private static $pdo;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$pdo = new \PDO('sqlite::memory:');
+        self::$pdo->exec('CREATE TABLE leiloes (
+            id INTEGER PRIMARY KEY,
+            descricao TEXT,
+            finalizado BOOL,
+            dataInicio TEXT
+        );');
+    }
+
+    protected function setUp(): void
+    {
+        self::$pdo->beginTransaction();
+    }
+
     public function testInsercaoEBuscaDevemFuncionar()
     {
+        // Arrange
         $leilao = new Leilao('Variant 0KM');
-        $leilaoDao = new LeilaoDao(ConnectionCreator::getConnection());
+        $leilaoDao = new LeilaoDao(self::$pdo);
 
         $leilaoDao->salva($leilao);
 
+        // Act
         $leiloes = $leilaoDao->recuperarNaoFinalizados();
 
+        // Assert
         self::assertCount(1, $leiloes);
         self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
         self::assertSame($leilao->recuperarDescricao(), $leiloes[0]->recuperarDescricao());
+    }
+
+    protected function tearDown(): void
+    {
+        self::$pdo->rollBack();
     }
 }
