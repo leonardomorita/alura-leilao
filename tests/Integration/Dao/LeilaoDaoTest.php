c3d50e10e0d4b626d78fa3efd6e0921104d58d8c
leonardomorita <leonardomorita2@gmail.com>
2026-01-21 22:00:19 -0300

    ✨ Aula 1 - Testando o banco de dados

---
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Alura\Leilao\Tests\Integration\Dao;
+
+use Alura\Leilao\Dao\Leilao as LeilaoDao;
+use Alura\Leilao\Infra\ConnectionCreator;
+use Alura\Leilao\Model\Leilao;
+use PHPUnit\Framework\TestCase;
+
+class LeilaoDaoTest extends TestCase
+{
+    public function testInsercaoEBuscaDevemFuncionar()
+    {
+        $leilao = new Leilao('Variant 0KM');
+        $leilaoDao = new LeilaoDao(ConnectionCreator::getConnection());
+
+        $leilaoDao->salva($leilao);
+
+        $leiloes = $leilaoDao->recuperarNaoFinalizados();
+
+        self::assertCount(1, $leiloes);
+        self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
+        self::assertSame($leilao->recuperarDescricao(), $leiloes[0]->recuperarDescricao());
+    }
+}
